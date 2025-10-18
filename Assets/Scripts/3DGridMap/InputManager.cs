8cb6ffaf44fe412bcf20dc9e82dc6850f77ec051
Christina Papadodimitraki <christina-v7@hotmail.com>
2025-10-18 22:50:58 +0200

    GridMap_Changes1

---
+++ Assets/Scripts/3DGridMap/InputManager.cs
@@ -0,0 +1,25 @@
+using UnityEditor;
+using UnityEngine;
+
+public class InputManager: MonoBehaviour
+{
+    [SerializeField] private Camera _sceneCamera;
+    [SerializeField] private LayerMask _placementLayerMask;
+
+    private Vector3 _LastPosition;
+
+    public Vector3 GetSelectedMapPosition()
+    {
+        Vector3 MousePos = Input.mousePosition;
+        MousePos.z = _sceneCamera.nearClipPlane;
+        Ray Ray = _sceneCamera.ScreenPointToRay(MousePos);
+        RaycastHit Hit;
+
+        if(Physics.Raycast(Ray,out Hit, 1000, _placementLayerMask))
+        {
+            _LastPosition = Hit.point;
+        }
+
+        return _LastPosition;
+    }
+}
