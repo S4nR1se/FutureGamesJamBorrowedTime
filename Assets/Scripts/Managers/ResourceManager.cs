550a10a2da5355492a3611b7bdf1d5289bc19344
Ilia Kapelin <kapelin.ilia@gmail.com>
2025-10-20 00:58:26 +0200

    ResourceManager
    Started on the resources system

---
+++ Assets/Scripts/Managers/ResourceManager.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : Manager
+{
+    [System.Serializable]
+    public class ResourceEntry
+    {
+        public Resources ResourceType;
+        public int Amount;
+    }
+
+    [SerializeField]
+    private List<ResourceEntry> _resourceList = new List<ResourceEntry>();
+
+    private Dictionary<Resources, int> _resources = new Dictionary<Resources, int>();
+
+    public override void Initialize()
+    {
+        _resources.Clear();
+        _resourceList.Clear();
+
+        foreach (Resources resource in Enum.GetValues(typeof(Resources)))
+        {
+            _resources[resource] = 0;
+            _resourceList.Add(new ResourceEntry { ResourceType = resource, Amount = 0 });
+        }
+    }
+
+    public void UpdateValue(Resources resource, int amount)
+    {
+        if (_resources.ContainsKey(resource))
+        {
+            _resources[resource] += amount;
+
+            ResourceEntry entry = _resourceList.Find(r => r.ResourceType == resource);
+            if (entry != null)
+            {
+                entry.Amount = _resources[resource];
+            }
+        }
+        else
+        {
+            Debug.LogWarning($"Resource {resource} not found in the dictionary.");
+        }
+    }
+
+    public int GetValue(Resources resource)
+    {
+        if (_resources.ContainsKey(resource))
+        {
+            return _resources[resource];
+        }
+        return 0;
+    }
+    private void OnValidate()
+    {
+        foreach (Resources resource in Enum.GetValues(typeof(Resources)))
+        {
+            if (!_resourceList.Exists(r => r.ResourceType == resource))
+            {
+                _resourceList.Add(new ResourceEntry { ResourceType = resource, Amount = 0 });
+            }
+        }
+        _resources.Clear();
+        foreach (var entry in _resourceList)
+        {
+            _resources[entry.ResourceType] = entry.Amount;
+        }
+    }
+}
+
+public enum Resources
+{
+    Materials,
+    FoodStock,
+    Graves,
+    Purr,
+    Dread
+}
