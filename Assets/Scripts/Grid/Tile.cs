49f132ca87e9340bdcf136e9de17d041a5800b0e
Ilia Kapelin <kapelin.ilia@gmail.com>
2025-10-21 00:22:32 +0200

    Updates

--- Assets/Scripts/Grid/Tile.cs
+++ Assets/Scripts/Grid/Tile.cs
@@ -1,59 +1,9 @@
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class Tile : MonoBehaviour, IInteractable
+public class Tile : MonoBehaviour
 {
     public TileType tileType;
-
-    private Renderer _renderer;
-    private Material _outlineMaterial;
-
-    private bool _isOutlineActive = false;
-
-    private void Awake()
-    {
-        _renderer = GetComponent<Renderer>();
-        if (_renderer.materials.Length > 1)
-            _outlineMaterial = _renderer.materials[1];
-        DisableOutline();
-    }
-    public void OnSelect()
-    {
-        
-    }
-    public void OnDeselect()
-    {
-        
-    }
-
-    public void OnHover()
-    {
-        EnableOutline();
-    }
-
-    public void OnHoverExit()
-    {
-        DisableOutline();
-    }
-    private void EnableOutline()
-    {
-        if (_outlineMaterial != null && !_isOutlineActive)
-        {
-            _isOutlineActive = true;
-
-            _outlineMaterial.SetFloat("_OutlineScale", 1.2f);
-        }
-    }
-
-    private void DisableOutline()
-    {
-        if (_outlineMaterial != null && _isOutlineActive)
-        {
-            _isOutlineActive = false;
-
-            _outlineMaterial.SetFloat("_OutlineScale", 0f);
-        }
-    }
 }
 public enum TileType
 {
