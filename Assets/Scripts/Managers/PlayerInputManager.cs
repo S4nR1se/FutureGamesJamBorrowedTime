7e40bd8f1aa9317a445598827769275db80ffcdf
Ilia Kapelin <kapelin.ilia@gmail.com>
2025-10-23 00:28:50 +0200

    Revert "uichanges"
    This reverts commit 92fa13049bd389c93e769817cd075bd96fbb0d3f.

--- Assets/Scripts/Managers/PlayerInputManager.cs
+++ Assets/Scripts/Managers/PlayerInputManager.cs
@@ -62,9 +62,8 @@ public class PlayerInputManager : Manager
         if (EventSystem.current != null && EventSystem.current.IsPointerOverGameObject())
             return;
 
-        
         Ray ray = _mainCam.ScreenPointToRay(Input.mousePosition);
-        
+
         HandleHover(ray);
         HandleClick(ray);
     }
