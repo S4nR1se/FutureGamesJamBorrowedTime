36ed4f5d216dcb11a1113ec8ef007be940c07398
Ilia Kapelin <kapelin.ilia@gmail.com>
2025-10-22 20:36:43 +0200

    BugFix

--- Assets/Scripts/NPC/Peasant.cs
+++ Assets/Scripts/NPC/Peasant.cs
@@ -147,33 +147,6 @@ public class Peasant : NPC, IWorker, IPeasant, IPoolable, IInteractable
     {
         _goToZoneBehaviour?.GoToZone();
     }
-
-    private void GoToZone(ZoneType zoneType)
-    {
-        if (_isTraveling)
-        {
-            return;
-        }
-
-        if (_occupiedZone != null)
-        {
-            _occupiedZone.Exit(this);
-            _occupiedZone = null;
-        }
-
-        Zone travelZone = GameManager.Instance.GetManager<ZoneManager>().GetRandomAvailableZone(zoneType);
-        if (travelZone == null)
-        {
-            return;
-        }
-
-        if (travelZone.TryEnter(this))
-        {
-            _reservedZone = travelZone;
-            SetGoToZoneBehaviour(travelZone);
-            _isTraveling = true;
-        }
-    }
     private void GoToZone(Zone travelZone)
     {
         if (_isTraveling)
@@ -235,7 +208,10 @@ public class Peasant : NPC, IWorker, IPeasant, IPoolable, IInteractable
             targetZone = GameManager.Instance.GetManager<ZoneManager>().GetRandomAvailableZone(Occupation.WorkZoneType);
         }
 
-        GoToZone(Occupation.WorkZoneType);
+        if (targetZone != null)
+        {
+            GoToZone(targetZone);
+        }
     }
     private void ValidatePreferredZone()
     {
