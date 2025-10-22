3395d190f2d0d90ad94b21cac9f846efa5336a1a
Ilia Kapelin <kapelin.ilia@gmail.com>
2025-10-22 19:59:42 +0200

    Fixed Occupation

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -132,10 +132,10 @@ public class UIManager : Manager
     public void DisplayNPCInfo(NPC npc)
     {
         _npcInfo.SetActive(true);
-        _npcNameText.text = "Name:" + npc.Name;
+        _npcNameText.text = "Name: " + npc.Name;
         if (npc is IWorker worker)
         {
-            _npcOccupationText.text = worker.Occupation.ToString();
+            _npcOccupationText.text = "Occupation: " + worker.Occupation.Title;
         }
         else
         {
