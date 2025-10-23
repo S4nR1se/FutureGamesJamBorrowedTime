e88c0b07480732e0b64b99d058973c7ab7017861
Meankel <mati.figura@gmail.com>
2025-10-23 16:26:49 +0200

    Old style Sound Connect / Adde few new sounds
    -depicated code, I will change it.
    -also add some sounds

--- Assets/Scripts/NPC/Skeleton.cs
+++ Assets/Scripts/NPC/Skeleton.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Skeleton : Undead
 {
+     public AudioClip RisingSkeletonSFX;
     public override void Initialize(Zone startZone, string name = "NPC", int lifeSpan = 10, float movementSpeed = 5, Occupation occupation = null, ZoneType restZoneType = ZoneType.Graveyard, DayCycle activeCycle = DayCycle.Night, SoundManager soundManager = null)
     {
         _soundManager = soundManager;
