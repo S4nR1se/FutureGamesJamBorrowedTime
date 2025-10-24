cce6d1c6bfdea113c1ce2f5399aff237a3cfb420
Christina Papadodimitraki <christina-v7@hotmail.com>
2025-10-24 13:04:48 +0200

    MainMenuChanges5

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -3,9 +3,26 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    AudioSource _mainMenuAudioSource;
+    [SerializeField] AudioClip _mainMenuMusic;
     private void Awake()
     {
         //SaveManager.save_instance.Load_Data();
+        SetUpAudio();
+    }
+
+    void SetUpAudio()
+    {
+
+        _mainMenuAudioSource = GetComponent<AudioSource>();
+        _mainMenuAudioSource.clip = _mainMenuMusic;
+        SoundManager.Instance.RegisterBackgroundMusic(_mainMenuAudioSource);
+
+        SoundManager.Instance.SetMasterVolume(SettingsManager.Instance.GetMasterAudioSliderVolume());
+        SoundManager.Instance.SetMusicVolume(SettingsManager.Instance.GetMusicAudioSliderVolume());
+        //SoundManager.Instance.SetSFXVolume(SettingsManager.Instance.GetSoundEffectsAudioSliderVolume());
+
+        SoundManager.Instance.PlayLoopingSound(_mainMenuAudioSource.clip, Vector3.one);
     }
 
     public void Start_Game()
